ee572919940ff6f41fab388da9c89251bc1e1f7c
Mingyi Li <me@limingyi.com>
2025-05-24 17:36:21 +0800

    Initial Commit

---
+++ Chronote/ChronoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChronoteApp.swift
+//  Chronote
+//
+//  Created by Isaac on 5/24/25.
+//
+
+import SwiftUI
+
+@main
+struct ChronoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChronoteTests/ChronoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChronoteTests.swift
+//  ChronoteTests
+//
+//  Created by Isaac on 5/24/25.
+//
+
+import Testing
+@testable import Chronote
+
+struct ChronoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
